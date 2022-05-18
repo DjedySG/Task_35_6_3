cb6184d489082e86395e9a4b027701d291581bb1
DjedySG <89001015+DjedySG@users.noreply.github.com>
2022-05-18 21:38:36 +0400

    Add files via upload

---
+++ Task_35_6_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <filesystem>
+
+
+auto recursiveGetFileNamesByExtension = [](const std::filesystem::path p, const std::string myExtension)
+{
+    for (auto& c : std::filesystem::recursive_directory_iterator(p))
+    {
+        if (c.path().extension() == myExtension)
+            std::cout << c.path() << '\n';
+    }
+};
+
+int main()
+{
+    std::string myExtension = ".exe";
+    std::filesystem::path p = "C:\\eclipse";
+    std::cout << p << std::endl;
+    for (auto& c : std::filesystem::recursive_directory_iterator(p))
+    {
+        if (c.path().extension() == myExtension)
+            std::cout << c.path() << '\n';
+    }
+    recursiveGetFileNamesByExtension(p, myExtension);
+}
+
